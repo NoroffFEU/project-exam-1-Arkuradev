4d30aeac25f58ec82927a17475dcb05a87dd3b32
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-10-28 19:40:40 +0100

    Footer
    The footer is complete.
    Added some root stylerules to make it easier to maintain and make changes later.

--- js/sideNav.js
+++ js/sideNav.js
@@ -3,10 +3,8 @@ function showSideNav() {
   const sideNav = document.querySelector(".sideNav");
   sideNav.style.display = "flex";
 }
-
+// function to hide the side menu when clicked.
 function hideSideNav() {
   const sideNav = document.querySelector(".sideNav");
   sideNav.style.display = "none";
 }
-
-// function to hide the side menu when clicked.
