7846c279df0899ac7326c931bd5c2c2ed74cfe9a
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2024-10-28 15:15:24 +0100

    Navbar
    Finished navigation and made it responsive for mobile

---
+++ js/sideNav.js
@@ -0,0 +1,12 @@
+// function to display the side menu when clicked.
+function showSideNav() {
+  const sideNav = document.querySelector(".sideNav");
+  sideNav.style.display = "flex";
+}
+
+function hideSideNav() {
+  const sideNav = document.querySelector(".sideNav");
+  sideNav.style.display = "none";
+}
+
+// function to hide the side menu when clicked.
